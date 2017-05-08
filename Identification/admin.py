1e48f656fa151e3418dce3df4f91a3bc9bd964e0
Willy MANGA <willy.manga@auf.org>
2017-05-08 11:21:40 +0000

    pas nécessaire à ce stade

--- Identification/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from Identification.models import Visite,  Usager, Service,PieceId
-
-# Register your models here.
-admin.site.register(Visite)
-admin.site.register(PieceId)
-admin.site.register(Service)
-admin.site.register(Usager)
--- Identification/static/Identifie/monscript.js
+++
@@ -1,5 +0,0 @@
-<script>
-window.addEventListener('load', function () {
-  console.log('Cette fonction est exécutée une fois quand la page est chargée.');
-});
-</script>
