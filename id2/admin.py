d22cb98457d88e307e7cd211baf94b6aab93aa46
Willy MANGA <willy.manga@auf.org>
2017-05-09 17:26:43 +0000

    présentation dans l'admin

--- id2/admin.py
+++ id2/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from id2.models import Visite,  Usager, Service,PieceId
 
+class PieceAdmin(admin.ModelAdmin):
+    list_display = ['typePiece','date_expiration']
+    list_filter = ['date_expiration','typePiece']
+    search_fields = ['code','typePiece']
+
 class UsagerAdmin(admin.ModelAdmin):
-    list_display = ('nom','prenom','sexe','email','telephone')
+    list_display = ('nom','prenom','sexe','email','telephone','piece')
     search_fields = ['nom','prenom']
+    list_filter = ['sexe']
 
 class VisiteAdmin(admin.ModelAdmin):
     list_display = ('usager','date_arrivee','date_deprt',
@@ -13,6 +19,6 @@ class VisiteAdmin(admin.ModelAdmin):
             ]
 
 admin.site.register(Visite,VisiteAdmin)
-admin.site.register(PieceId)
+admin.site.register(PieceId,PieceAdmin)
 admin.site.register(Service)
 admin.site.register(Usager,UsagerAdmin)
