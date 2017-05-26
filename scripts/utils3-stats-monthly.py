1184668349ea4e7e1edeb58cce272d2c90ec8b14
Willy MANGA <willy.manga@auf.org>
2017-05-26 16:00:13 +0000

    stats mensuels: corrigeons d'abord ceci

--- scripts/utils3-stats-monthly.py
+++ scripts/utils3-stats-monthly.py
@@ -89,73 +89,83 @@ def collecte():
 
 # section envoi courriel
 
+def envoiStats(s_smtp,from_addr,to_addrs,sujet,fuseau,dacgl_stats):
+    """
+    Envoi des stats à une liste interne
+    """
+
+    moment = datetime.now(tzone(fuseau)).\
+        strftime('%a, %d %B %Y %H:%M:%S %z')
+    #create message container
+
+    msg =  MIMEMultipart('alternative')
+    msg['Subject'] = sujet
+    msg['From'] = from_addr
+    msg['To'] = to_addrs
+    msg['Date'] = moment
+
+    #create the body of the message
+    message = u"""
+    Statistiques des visites a la DACGL pour le mois de %s:\n
+
+    = Nombre d'usagers enregistrés = \n
+     * Au total : %s \n
+     * Pour le mois: %s\n
+
+    = Nombre de visites enregistrées = \n
+
+    Details a l'adresse http://utils2.cm.auf.org/.
+             """ % (dacgl_stats[usagers_enreg['TOTAL']],
+                     dacgl_stats[usagers_enreg['Courant']],
+                     )
+
+            
+    message_html = """\
+            <html>
+            <head><title>Test</title></head>
+            <body>
+             <h3>Frequence des visites a la DACGL pour la semaine courante</h3>
+              <p>Date du jour: %s </p>
+            <ul>
+             <li>Total des visites: %s</li>
+                 <li>Visites au CNF: %s</li>
+                 <li>Visites au fablab: %s</li>
+             <li>Total des usagers enregistres: %s</li>
+            </ul>
+            <p>Details a l'adresse <a href="http://utils2.cm.auf.org/">
+            http://utils2.cm.auf.org/</a>.
+            </body>
+            </html>
+            """ % (date_jour,visit_semaine.count(),
+                     visit_service['CNF'].count(),
+                     visit_service['fablab'].count(),
+                     usager_total,
+                     )
+
+    #record the MIME types
+    part1 = MIMEText(message, 'plain')
+    part2 = MIMEText(message_html, 'html')
+
+    #attach parts into message container
+    msg.attach(part1)
+    msg.attach(part2)
+
+    #send
+    client = smtplib.SMTP(s_smtp)
+    try:
+            client.sendmail(from_addr,to_addrs,msg.as_string())
+            return 'stats fréquentations mensuelles DACGL envoyées'
+    except Exception, err:
+        return "Oups :-("
+
+
+fuseau = 'Africa/Douala'
 s_smtp = "smtp.cm.auf.org"
 from_addr = u'technique@cm.auf.org'
-to_addrs = 'cnfy@cm.auf.org,ongolafablab@cm.auf.org'
-sujet = 'Informations sur les visites a la DACGL: semaine %s' % \
-        semaine_courante
+to_addrs = 'diffusion-bureau@cm.auf.org'
+sujet = 'Informations sur les visites a la DACGL: Mois de %s' % \
+        (t_debut_mois_prec.strftime(%B))
 
-moment = datetime.now(tzone('Africa/Douala')).\
-    strftime('%a, %d %B %Y %H:%M:%S %z')
-#create message container
+#envoiStats(s_smtp,from_addr,to_addrs,sujet,dacgl_stats)
 
-msg =  MIMEMultipart('alternative')
-msg['Subject'] = sujet
-msg['From'] = from_addr
-msg['To'] = to_addrs
-msg['Date'] = moment
-
-#create the body of the message
-message = u"""
-Frequence des visites a la DACGL pour la semaine courante:\n
-Date du jour : %s \n
- * Total de visites : %s\n
-  * Visites au CNF : %s\n
-  * Visites au Fablab: %s\n
-
- * Nombre d'usagers enregistres au total : %s\
-
-Details a l'adresse http://utils2.cm.auf.org/.
-         """ % (date_jour,visit_semaine.count(),
-                 visit_service['CNF'].count(),
-                 visit_service['fablab'].count(),
-                 usager_total,
-                 )
-
-message_html = """\
-        <html>
-        <head><title>Test</title></head>
-        <body>
-         <h3>Frequence des visites a la DACGL pour la semaine courante</h3>
-          <p>Date du jour: %s </p>
-        <ul>
-         <li>Total des visites: %s</li>
-             <li>Visites au CNF: %s</li>
-             <li>Visites au fablab: %s</li>
-         <li>Total des usagers enregistres: %s</li>
-        </ul>
-        <p>Details a l'adresse <a href="http://utils2.cm.auf.org/">
-        http://utils2.cm.auf.org/</a>.
-        </body>
-        </html>
-        """ % (date_jour,visit_semaine.count(),
-                 visit_service['CNF'].count(),
-                 visit_service['fablab'].count(),
-                 usager_total,
-                 )
-
-#record the MIME types
-part1 = MIMEText(message, 'plain')
-part2 = MIMEText(message_html, 'html')
-
-#attach parts into message container
-msg.attach(part1)
-msg.attach(part2)
-
-#send
-client = smtplib.SMTP(s_smtp)
-try:
-        client.sendmail(from_addr,to_addrs,msg.as_string())
-        print 'stats fréquentations DACGL envoyées'
-except Exception, err:
-    print "Oups :-("
+infos = collecte()
