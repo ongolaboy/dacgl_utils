4ff4a232ef9967e84dd77fc06420f95d43e8aaaa
Willy MANGA <willy.manga@auf.org>
2017-10-22 23:15:35 +0100

    premier jet template ok

--- dacgl/urls.py
+++ dacgl/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     #url(r'^$', 'dacgl.views.home', name='home'),
+    url(r'^inventaire2/$', include('inventaire2.urls')),
     url(r'^ident/', include('id2.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- inventaire2/views.py
+++ inventaire2/views.py
@@ -1,3 +1,28 @@
+import csv
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Piece
+
 # Create your views here.
+
+def index(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="sortie.csv"'
+    
+    total_piece = Piece.objects.all()
+    writer = csv.writer(response)
+
+    writer.writerow(['Intitule','date_acquisition',
+            'code_inventaire','commande_coda'])
+    for piece in Piece.objects.all():
+        writer.writerow([piece.intitule, piece.date_acquisition,
+                piece.code_inventaire, piece.commande_coda])
+
+#    writer.writerow(['Premiere ligne','un','deu','trois'])
+#    writer.writerow(['deuxieme ligne','cinquo','deu','mais ou ai je la tete'])
+    return response
+#    total_piece = Piece.objects.all()
+#    return render(request,'inventaire2/index.html',
+#            {'total_piece': total_piece})
