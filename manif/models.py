78b3dc9658dd0fba0550f89c7cf7c6c29b8f1e8a
Willy MANGA <willy.manga@auf.org>
2017-11-03 05:25:44 +0100

    manif:deux dates pour avoir la période

--- manif/models.py
+++ manif/models.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from django.db import models
 from django.utils import timezone
 
@@ -8,10 +10,17 @@ class Evenement(models.Model):
     lieu = models.CharField(max_length=150)
     intitule = models.CharField(max_length=200)
     description = models.TextField(blank=True)
-    periode = models.DateField()
-    duree = models.CharField(max_length=15,blank=True)
+    debut = models.DateField()
+    fin = models.DateField(blank=True,null=True)
     participant = models.ManyToManyField(Usager, through='Participation')
     ouvert = models.BooleanField(default=True)
+
+    def duree(self):
+        if self.fin is None:
+            self.periode = timedelta(days=1)
+        else:
+            self.periode = (self.fin - self.debut).days
+        return self.periode
 
     def __str__(self):
         return "%s  (%s)" % (self.intitule, self.lieu)
