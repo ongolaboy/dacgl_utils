d0dd65f1106be1c86f4470419030f758b459aa47
Willy MANGA <willy.manga@auf.org>
2018-01-18 07:09:46 +0100

    script visite: ok pour les mois

--- scripts/visites-infos2.py
+++ scripts/visites-infos2.py
@@ -3,10 +3,14 @@
 Collecter des données issues des visites pour une année.
 
 Exemple d'usage
-"python3 visites-infos2.py <annee>"
+"python3 visites-infos2.py [annee] [mois]"
 
 L'année peut être donnée en paramètre au si elle est absente
 le script considèrera l'année en cours.
+
+Le mois si fourni, le script ne ressortira que les infos du mois
+indiqué. Nombre entre 1-12
+
 En sortie le script génère un fichier au format html contenant un
 tableau. Ce dernier pourra aussi être envoyé par courriel.
 """
@@ -26,6 +30,8 @@ from django.utils import timezone
 
 BASE_DIR = '/home/willy/lab/django/dacgl_utils'
 SITE = 'Bureau régional'
+ACCUEIL_URL = "https://utils3.cm.auf.org/ident/"
+ACCUEIL = "Page d'accueil {0}".format(ACCUEIL_URL)
 sys.path.append(BASE_DIR)
 os.environ["DJANGO_SETTINGS_MODULE"] = "dacgl.settings"
 
@@ -33,53 +39,8 @@ os.environ["DJANGO_SETTINGS_MODULE"] = "dacgl.settings"
 django.setup()
 
 from dacgl.settings import TIME_ZONE
-from id2.utils import periodes
+from id2.utils import periodes,collecte
 from id2.models import Visite,Usager,Service
-
-def collecte(annee,mois=''):
-    """
-    Collecte des données sur une année donnée
-
-    La sortie est une liste contenant:
-
-    * le nombre d'usagers enregistrées sur l'année
-
-    * les infos sur les visites
-     * elles sont présentées par service dans un dictionnaire
-       où les clés des tuples (service,mois)
-       ex: visite[('CNF',11)] renvoie un querySet correspondant
-       aux visites effectuées dans le CNF en Novembre
-
-    * la liste des services
-    """
-
-
-    dacgl_stats = []
-    p = periodes(annee,mois)
-
-
-    services = []
-    usagers_enreg = {}
-    visit = {}
-    for s in Service.objects.all():
-        services.append(s.nom_serv)
-
-    v_Total =\
-            Visite.objects.filter(date_arrivee__range=(p[1][0],p[12][1]))
-    for m in range(1,13):
-        for v in services:
-            v0 = v_Total.filter(service__nom_serv=v)
-            visit[(v,m)] = []
-            visit[(v,m)].append(v0.filter(\
-                    date_arrivee__range=(p[m][0],p[m][1])))
-
-    usagers_enreg = Usager.objects
-
-    dacgl_stats = [usagers_enreg,visit,services]
-
-    return dacgl_stats
-
-#Commençons donc à collecter les infos
 
 #on récupère l'année fournie en paramètre le cas échéant
 try :
@@ -87,7 +48,13 @@ try :
 except IndexError:
     annee = datetime.now().year
 
-stats = collecte(annee)
+#éventuellement aussi le mois
+try :
+    mois = int(sys.argv[2])
+except IndexError:
+    mois = ''
+
+stats = collecte(annee,mois)
 
 usager_Enregistre = stats[0].count()
 Visites = stats[1]
@@ -97,16 +64,17 @@ lieu = stats[2]
 lieu.sort() #on trie par ordre alphabétique la liste des services
 nbrservice = len(lieu)
 skel_ligne = ''
+affichage_mois = ''
+premierTour = True #pour distinguer la première colonne des autres
 
-for m in range(1,13):
-    mois = m
+if mois != '':
+    affichage_mois = '<p>Mois:{0}</p>'.format(mois)
     vTotalMois = 0
     skel_service = ''
     cellules = nbrservice
-    premierTour = True #pour distinguer la première colonne des autres
     for s in lieu:
         service = s
-        v_par_service = Visites[s,m][0] #visite par service
+        v_par_service = Visites[s,mois][0] #visite par service
         try:
             vTotal = v_par_service.count()
             vDistinct = v_par_service.values('usager').distinct().count()
@@ -149,6 +117,58 @@ for m in range(1,13):
     </tr>
     """.format(nbrservice,mois,skel_service)
 
+else :
+    for m in range(1,13):
+        mois = m
+        vTotalMois = 0
+        skel_service = ''
+        cellules = nbrservice
+        premierTour = True #pour distinguer la première colonne des autres
+        for s in lieu:
+            service = s
+            v_par_service = Visites[s,m][0] #visite par service
+            try:
+                vTotal = v_par_service.count()
+                vDistinct = v_par_service.values('usager').distinct().count()
+            except IndexError:
+                vTotal = 0
+                vDistinct = 0
+
+            vTotalMois += vDistinct
+            if premierTour == True:
+                skel_service =\
+                        """
+                        <td>{0}</td>
+                            <td>{1}</td>
+                            <td>{2}</td>
+                            </tr>
+                        """.format(service,
+                            vTotal,
+                            vDistinct,
+                            )
+                premierTour = False
+            else :
+
+                skel_service +=\
+                        """
+                        <tr>
+                        <td>{0}</td>
+                            <td>{1}</td>
+                            <td>{2}</td>
+                            </tr>
+                        """.format(service,
+                            vTotal,
+                            vDistinct,
+                            )
+
+        skel_ligne += \
+        """
+        <tr>
+        <td rowspan="{0}">{1}</td>
+        {2}
+        </tr>
+        """.format(nbrservice,mois,skel_service)
+
 #Les ossatures
 tableau =\
 """
@@ -165,20 +185,25 @@ tableau =\
 
 skel0 =\
     """
+<!DOCTYPE html>
 <html>
 
   <head>
+   <meta http-equiv="content-type" content="text/html; charset=utf-8" />
    <title> Stats sur la frequentation - {0}</title>
   </head>
 
   <body>
     <h2> Année {1} </h2>
-    <p>Nombre d'usagers enregistrés au total dans l'année: {2}
+    {4}
+    {5}
+    <p>Nombre total d'usagers enregistrés toute période confondue: {2}
     <br />
     {3}
   </body>
 </html>
-    """.format(SITE,annee,usager_Enregistre,tableau)
+    """.format(SITE,annee,usager_Enregistre,tableau,
+            ACCUEIL,affichage_mois)
 
 #sortie = Template(skel0)
 #TODO à présenter ainsi plus tard quand ça sera ok
